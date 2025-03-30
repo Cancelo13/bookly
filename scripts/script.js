b223f9550ac44c0da705437a2d40667f0481e814
Cancelo13 <mohamedmagdy6674@gmail.com>
2025-03-30 22:46:04 +0200

    Init : Script - Login - Register , Style Main

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+const errorMessage = document.getElementById("error-message");
+const successMessage = document.getElementById("success-message");
+const form = document.getElementById("login-form");
